99911666bca90c382e6d8067c0fd81708b52698f
yzt <609984807@qq.com>
2021-01-14 10:44:17 +0800

    初始项目

---
+++ src/Base/Test.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Captainbi\Hyperf\Base;
+
+class Test
+{
+    /**
+     * 构造方法
+     * @access public
+     * @param App $app 应用对象
+     */
+    public function __construct()
+    {
+        
+    }
+
+
+    public function test()
+    {
+        return "hello";
+    }
+
+}
